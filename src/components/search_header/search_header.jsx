ede391b350687214d551fecf7bc92add24be1afd
kimyw2357 <bubuchachakk@gmail.com>
2022-08-04 17:48:49 +0900

    make search header1

---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,11 @@
+import styles from './search_header.module.css'
+import React from 'react';
+
+const SearchHeader = (props) => {
+  return (
+    <h1>
+      head
+    </h1>
+  )
+}
+export default SearchHeader;
